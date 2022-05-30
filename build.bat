fa4f5e76bfa3b115fab474b18b9986d8c6348fc9
shaggyze <shaggyze@gmail.com>
2022-05-29 19:05:53 -0700

    Update to 1.3.2
    Update to 1.3.2

---
+++ build.bat
@@ -0,0 +1,4 @@
+cargo build --release
+copy /Y "C:\Users\%username%\Documents\GitHub\mabi-pack2\target\release\mabi-pack2.exe" "C:\Nexon\Library\mabinogi\appdata\mabi-pack2\mabi-pack2.exe"
+copy /Y "C:\Users\%username%\Documents\GitHub\mabi-pack2\target\release\mabi-pack2.exe" "C:\Nexon\Library\mabinogi\appdata\package\mabi-pack2.exe"
+copy /Y "C:\Users\%username%\Documents\GitHub\mabi-pack2\target\release\mabi-pack2.exe" "C:\Users\Shaggy\Documents\GitHub\uotiara\Tiara's Moonshine Mod\Tools\mabi-pack2\mabi-pack2.exe"
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ mod pack;
 
 fn main() {
     let args = Command::new("Mabinogi pack utilities 2")
-        .version("v1.3.1")
+        .version("v1.3.2")
         .author("regomne <fallingsunz@gmail.com>")
         .subcommand(
             Command::new("pack")
